2cecaaf27094b45822b3cf1e11b5ce590595d2fd
liosyafriza <80315321+liosyafriza@users.noreply.github.com>
2021-03-23 17:23:16 +0700

    minggu 3

---
+++ Minggu 3/src/Array.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        String[] siswa = {"ibnu", "lio", "boy"};
+        //index [] = 0
+        System.out.println(siswa[0]);
+        System.out.println(siswa[1]);
+        System.out.println(siswa[2]);
+    }
+}
